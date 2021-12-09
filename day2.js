94ffe7db53677dba7b96a3f280fa2710aa8a7cd8
lennartdeknikker <lennart@momkai.com>
2021-12-09 19:04:09 +0100

    add solution day 2

--- day2.js
+++ day2.js
@@ -1,7 +1,32 @@
-import getDataForDay from './getData.js';
+import getDataForDay from './getData.js'
 
 getDataForDay(2)
-  .then(data => {
-
-  }
-)
+.then(text => text.split("\n"))
+.then(dataArray => {
+console.log('🚀 ~ dataArray', dataArray)
+  let depth = 0
+  let xPos = 0
+  dataArray.forEach(item => {
+    const number = Array.from(item).pop()
+    if (item.includes('forward')) xPos += parseInt(number)
+    if (item.includes('up')) depth -= parseInt(number)
+    if (item.includes('down')) depth += parseInt(number)
+  })
+  console.log('first part: ', depth * xPos)
+  return dataArray
+})
+.then(dataArray => {
+  let aim = 0
+  let depth = 0
+  let xPos = 0
+  dataArray.forEach(item => {
+    const number = Array.from(item).pop()
+    if (item.includes('forward')) {
+      xPos += parseInt(number)
+      depth += number * aim
+    }
+    if (item.includes('up')) aim -= parseInt(number)
+    if (item.includes('down')) aim += parseInt(number)
+  })
+  console.log('second part: ', depth * xPos)
+})
